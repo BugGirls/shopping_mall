70dfcf65fe89a976d89af162a8cb8a269873b537
buggirl <97363829@qq.com>
2017-05-30 22:04:21 +0800

    project init commit

---
+++ src/main/java/com/girl/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.girl.dao;
+
+import com.girl.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
